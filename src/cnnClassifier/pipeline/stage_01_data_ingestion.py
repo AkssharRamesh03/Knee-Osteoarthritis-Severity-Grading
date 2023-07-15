916adb19663df0d19b2746460b800ca5c4b3328e
AkssharRamesh03 <rmshakshar@gmail.com>
2023-07-16 00:40:14 +0100

    data ingestion.

---
+++ src/cnnClassifier/pipeline/stage_01_data_ingestion.py
@@ -0,0 +1,28 @@
+from src.cnnClassifier import logger
+from src.cnnClassifier.components.data_ingestion import DataIngestion
+from src.cnnClassifier.config.configuration.configuration import ConfigurationManager
+
+STAGE_NAME = "DATA INGESTION STAGE"
+
+
+class DataIngestionTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        data_ingestion_config = config.get_data_ingestion_config()
+        data_ingestion = DataIngestion(config=data_ingestion_config)
+        data_ingestion.download_file()
+        data_ingestion.extract_zip_file()
+
+
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>>>> STAGE \"{STAGE_NAME}\" STARTED <<<<<<")
+        obj = DataIngestionTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> STAGE \"{STAGE_NAME}\" COMPLETED <<<<<<\n\nx============x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
